ff7793fc71bcbf82e03b2ed98968041e52aa521a
Billy Moore <bm1255@york.ac.uk>
2024-03-13 12:03:39 +0000

    Fixed local dependancies in HesligtonEast.tmx (the main map)

--- assets/Tilesets/Water.tsx
+++ assets/Tilesets/Water.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Water" tilewidth="8" tileheight="8" tilecount="16" columns="8">
- <image source="Sprout Lands - Sprites - Basic pack/Sprout Lands - Sprites - Basic pack/Tilesets/Water.png" width="64" height="16"/>
+ <image source="Water.png" width="64" height="16"/>
 </tileset>
